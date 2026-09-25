644f3ac7ba6a1a2aa2167e16b60be980b6c51350
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-09-25 20:10:26 +0600

    practice object

---
+++ task2.ts
@@ -0,0 +1,40 @@
+const skills: string[] = ["React", "Next.js", "Node.js", "MongoDB"];
+
+const experience: number[] = [1, 2, 3, 4, 5];
+
+
+const userId: (string | number)[] = ['abcd123', 123] 
+
+
+const user: (string | number)[] = ["Abdullah", 25, "Saba", 30]; 
+
+// name → "Laptop"
+// price → 80000
+// inStock → true
+
+
+const product : {
+    name: string, 
+    price: number,
+    inStock: boolean
+} = {
+    name: "Computer",
+    price: 200,
+    inStock: true 
+
+}
+
+
+
+
+const developer: {
+    name: string,
+    age: number,
+    skills: string[],
+    remote:boolean
+} = {
+  name: "Abdullah",
+  age: 25,
+  skills: ["React", "Next.js", "Node.js"],
+  remote: true,
+};
